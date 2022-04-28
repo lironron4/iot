31a102c4047f6ad002c560ea03980788e6ac8b97
lironron4 <96688976+lironron4@users.noreply.github.com>
2022-04-28 13:22:59 +0300

    Create controller.hpp

---
+++ controller.hpp
@@ -0,0 +1,39 @@
+#ifndef CONTROLLER_AGENT_HPP
+#define CONTROLLER_AGENT_HPP
+
+#include <set>
+#include <string>
+#include "smart_event.hpp"
+//?
+
+using namespace std;
+
+namespace iot
+{
+
+class ControllerAgent : public Agent
+{
+public:
+   //or std::pair<room, floor> ??
+   ControllerAgent(string a_id, string a_type, string a_floor, string a_room, string a_config );
+   virtual ~ControllerAgent() {};
+   
+    string id() const;
+    string eventType() const;
+    string floor() const;
+    string room() const;
+    string config() const;
+    
+    void getEvent(iot::Event a_event);
+    virtual void react(iot::Event a_event);    
+
+private:
+    string m_id;
+    string m_eventType;
+    string m_floor;
+    string m_room;
+    string m_config;
+};
+
+}// namespace iot
+#endif
