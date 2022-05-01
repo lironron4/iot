399ec6d7c0f5b30816dd1eb12f0a7bed9bfee0fa
lironron4 <96688976+lironron4@users.noreply.github.com>
2022-05-01 19:08:47 +0300

    Update smart_event.hpp

--- smart_event.hpp
+++ smart_event.hpp
@@ -1,68 +1,97 @@
+#include <string>
+#include <ctime>
+#ifndef EVENT_HPP
+#define EVENT_HPP
 
+namespace iot{
 
-
-//created by sensor
-//server gets buffer with event in it - turned back into event class
-
-class Event {
+class Event{
 public:
-    Event();
-    ~Event();
+        Event();
+        ~Event();
+
 protected:
-    time_t m_timeStamp;
-    std::string m_eventType;
-    std::string m_location; //class Location??
-//private:
-    //cctor ?
-    //operator= ?
+        time_t getTime() const;
+        std::string getLocation() const;
+        time_t m_when;
+        std::string m_where; // class Location??
+        bool isHappening() const;
+
+private:
+        Event &operator=(Event const &a_orig); // no implementation
+        Event(Event const &a_orig);            // no implementation
 };
 
-//how to put into log? 
-
+    // how to put into log?
 
 class Temperature : public Event{
 public:
-    Temperature(double a_detection, bool isRising);
-private:
-    double m_currTemp;
-}; 
+        Temperature(std::string a_where, float a_temp);
+        float getTemp() const;
 
-//create rand temp simulator; 
+private:
+    double m_content;
+    std::string m_where;
+    time_t m_when;
+};
+
+// create rand temp simulator;
 
 class Fire : public Event{
 public:
-    Fire();
-}; 
+    Fire(std::string a_where);
+    bool isHappening() const;
+
+private:
+    bool m_content;
+    std::string m_where;
+    time_t m_when;
+};
 
 class Smoke : public Event{
 public:
-    Smoke();
-}; 
+    Smoke(std::string a_where);
+    bool isHappening() const;
 
+private:
+    bool m_content;
+    std::string m_where;
+    time_t m_when;
+};
 
+    // sensor creates event.
+    // sensor:
+    // knows its location,
+    // knows what event type it measures,
+    //
 class Movement : public Event{
 public:
-    Movement();
-}; 
+    Movement(std::string a_where);
+    bool isHappening() const;
 
-// class EventMap{ //KNOWS BUILDING - can go to certain location in building
-//     //a map of key: event, value: func
-//     //key: fire, // value: extinguish(event.m_location) >>> how is this done?? predicate function??
-// };
+private:
+    bool m_content;
+    std::string m_where;
+    time_t m_when;
+};
 
-//eventMap[event.m_eventType](event.m_location);
+    // class EventMap{ //KNOWS BUILDING - can go to certain location in building
+    //     //a map of key: event, value: func
+    //     //key: fire, // value: extinguish(event.m_location) >>> how is this done?? predicate function??
+    // };
 
+    // eventMap[event.m_eventType](event.m_location);
 
+    // class EventHandler{
+    // public:
+    //     void handle(Event m_event);
+    // private:
+    //     //container m_events
+    // };
 
-// class EventHandler{
-// public: 
-//     void handle(Event m_event);
-// private:
-//     //container m_events
-// };
+    // A smart home controller, smart home hub, or home automation controller is the brain behind your smart home. It is the device that controls all your smart appliances and devices. These can range anywhere from lightbulbs to music systems, electronic blinds, TVs, doorbells, thermostats, security systems and so much more.
 
-//A smart home controller, smart home hub, or home automation controller is the brain behind your smart home. It is the device that controls all your smart appliances and devices. These can range anywhere from lightbulbs to music systems, electronic blinds, TVs, doorbells, thermostats, security systems and so much more.
+    // fire event - turn to sparkAlexa? she can find the specific fire extinguisher by location?
 
-
-//fire event - turn to sparkAlexa? she can find the specific fire extinguisher by location?
-
+} // namespace iot
+#endif // EVENT_HPP
